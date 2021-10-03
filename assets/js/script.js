a4a06d464440307f48f1300744067297f77ea115
sehaj97 <sehajmagan@gmail.com>
2021-10-03 08:32:15 -0400

    added handle for clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,13 @@ var searchList = $("<ul>").addClass("list-group history-list-items");
 varlistItem="";
 inputNewValue = "";
 inputOldValue = "";
+counter = 0;
 var formSubmitHandler = function(event) {
-    // prevent page from refreshing
+    
     event.preventDefault();
     if(cityInput.val() != ""){
         inputNewValue = cityInput.val();
-        listItem = $("<button>").addClass("list-group-item btn text-white bg-secondary text-center mt-2 forecast-city").text(cityInput.val()).val(inputNewValue);
+        listItem = $("<button>").addClass("list-group-item text-white bg-secondary text-center mt-2 forecast-history-" + counter).text(cityInput.val()).val(inputNewValue);
         if($(".history-list-items button") === undefined){
             inputOldValue = "";
         }
@@ -18,13 +19,25 @@ var formSubmitHandler = function(event) {
             $(".history-list-items").prepend(listItem);
             inputOldValue = $(".history-list-items button")[0].value;
             if($(".history-list-items button").length > 8){
-                $(".list-group-item").last().remove()
+                $(".list-group-item").last().remove();
+                counter = 0;
             }
-            //click to handle new requestr
-        } 
+        }
+        
+        getApiInfo(cityInput.val());
+        $('body').on('click', ".forecast-history-" + counter, function() {
+            getApiInfo(inputNewValue);
+        });
+        cityInput.val("");
+        counter++;
+
     } else {
      alert("please provide city!");
     }
+};
+
+function getApiInfo(value) {
+    console.log(value);
 };
 
 searchHistory.html("");
